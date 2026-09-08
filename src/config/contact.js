271dc262210f56029a0bab00bc1645df013b929d
eshaghaffar011-ctrl <eshaghaffar011@gmail.com>
2026-09-08 15:05:05 +0500

    update HK FITTERS info

--- src/config/contact.js
+++ src/config/contact.js
@@ -1,12 +1,12 @@
 export const contactInfo = {
   companyName: 'HK FITTERS',
-  whatsappNumber: '+92 310 7983488',
+  whatsappNumber: '',
   whatsappMessage: 'Hello HK FITTERS,\nI am interested in your products.\nPlease share your catalogue and export details.',
-  businessEmail: 'hkfitters23@gmail.com',
+  businessEmail: 'hkfitters11111@gmail.com',
   businessAddress: 'Pakistan • Global Export',
   businessHours: 'Monday - Saturday: 9:00 AM - 6:00 PM',
-  facebook: 'https://www.facebook.com/share/1ETndC33at/',
-  instagram: 'https://www.instagram.com/hk.fitters?igsh=MW00NWE4ZzdzMm0yYw==',
+  facebook: '',
+  instagram: '',
   linkedin: '',
 };
 
